d0b6dc6307293c7d00c1e75760ff0fd1e16c75d7
rdubisz <rdubisz@gmail.com>
2025-10-30 14:39:20 +0000

    Lessons refactoring

--- scripts/lesson_scene_numbers_0_100.gd
+++ scripts/lesson_scene_numbers_0_100.gd
@@ -20,7 +20,7 @@ var lesson :ChooseFromNineLesson
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print(OS.get_data_dir())
-	lesson = ChooseFromNineLesson.new(10, "Numbers 1-10", "numbers_1in9_data", 9, 10)
+	lesson = ChooseFromNineLesson.new(40, "Numbers 0-100", "numbers_1in9_data", 9)
 	print(str(lesson.steps))
 	draw_step(lesson.current_step_number)
 
